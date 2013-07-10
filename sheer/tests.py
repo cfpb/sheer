e52a5800ec2564d4d6b7998fa5f402aa81c56469
Ross Karchner <ross.karchner@cfpb.gov>
2013-07-10 17:04:14 -0400

    Another test!

--- sheer/tests.py
+++ sheer/tests.py
@@ -3,9 +3,15 @@ import os.path
 from sheer import reader
 from sheer.utility import get_case_contents
 
-class testFrontmatter:
+class testReader:
     def test_frontmatter_extraction(self):
         data = get_case_contents('simple_frontmatter.txt')
         frontmatter, text = reader.extract_frontmatter(data)
         assert(frontmatter == "\nHi, I'm Frontmatter\n")
         assert("I'm a unicorn!" in text)
+
+    def test_no_frontmatter(self):
+        data= get_case_contents('no_frontmatter.txt')
+        frontmatter, text = reader.extract_frontmatter(data)
+        assert(frontmatter == None)
+        assert("I don't have frontmatter at all" in text)
