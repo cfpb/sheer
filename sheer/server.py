371228aeeae3ec0675a7a580e42a43e19dae38d9
Ross Karchner <ross.karchner@cfpb.gov>
2013-11-21 13:51:22 -0500

    removed an unused function

--- sheer/server.py
+++ sheer/server.py
@@ -9,30 +9,6 @@ from sheer import reader, query, exceptions, utility, templates
 from sheer.site import Site
 
 
-def response_for_path(physical_path, request):
-    base, ext = os.path.splitext(physical_path)
-    canonical_ext=ext.lower()
-    
-    response= Response(content_type='text/html')
-
-    if ext:
-        mime = mimetypes.types_map.get(canonical_ext)
-        if type:
-            response.headers['Content-type']= mime
-
-    if canonical_ext not in ['.html']:
-        response.body= file(physical_path, 'rb').read()
-
-    else:
-        response.charset='utf-8'
-        response.text = render_html(physical_path, request)
-
-
-    return response
-
-        
-
-
 def serve_wsgi_app_with_cli_args(args):
         root_dir = os.path.realpath(args.location)
         site = Site(root_dir)
