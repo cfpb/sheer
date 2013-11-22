dbe568efdddc9777983573d080260c0d0c010cf2
Ross Karchner <ross.karchner@cfpb.gov>
2013-11-22 12:13:48 -0500

    exploring how to bootstrap flask

--- sheer/server.py
+++ sheer/server.py
@@ -2,6 +2,8 @@ import os.path, os
 import mimetypes
 import logging
 
+from flask import Flask
+
 import markdown
 
 
@@ -9,13 +11,23 @@ from sheer import reader, query, exceptions, utility, templates
 from sheer.site import Site
 
 
+def create_flask_handlers(app):
+    for path in os.listdir('.'):
+        if path[0] in ['.','_']:
+            continue
+        if os.path.isdir(path):
+            for root, dirs, files in os.walk('path'):
+                print root
+
 def serve_wsgi_app_with_cli_args(args):
         root_dir = os.path.realpath(args.location)
+        os.chdir(root_dir)
         site = Site(root_dir)
-        application =  site.handle_wsgi
+        app = Flask(__name__.split('.')[0])
         if args.debug:
-            from werkzeug.debug import DebuggedApplication
-            application = DebuggedApplication(application, evalex=True)
+            app = Flask(__name__.split('.')[0], debug=True)
+
+        create_flask_handlers(app)
 
         from paste import httpserver
-        httpserver.serve(application, host='127.0.0.1', port=args.port)
+        httpserver.serve(app, host='127.0.0.1', port=args.port)
