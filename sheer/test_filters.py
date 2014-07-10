a055d845da8d4c1e9c2a63c43e58eb7bcfe35869
Dan Ford <dpford@gmail.com>
2014-07-10 11:24:34 -0400

    Valdiation for date range filter

--- sheer/test_filters.py
+++ sheer/test_filters.py
@@ -30,3 +30,17 @@ class testArgParsing(object):
         selected = filters.selected_filters_from_multidict(self.args, 'category')
         assert (('cats') in selected)
         assert (('dogs') in selected)
+
+    def test_date_validation_incorrect_range(self):
+        args = MultiDict([('filter_range_date_gte', '2014-6'),
+                          ('filter_range_date_lte', '2013-6')])
+        filter_dsl = filters.filter_dsl_from_multidict(args)
+        assert(filter_dsl['and'][0]['range']['date']['gte'] == '2013-6')
+        assert(filter_dsl['and'][0]['range']['date']['lte'] == '2014-6')
+
+    def test_date_validation_correct_range(self):
+        args = MultiDict([('filter_range_date_gte', '2013-6'),
+                          ('filter_range_date_lte', '2014-6')])
+        filter_dsl = filters.filter_dsl_from_multidict(args)
+        assert(filter_dsl['and'][0]['range']['date']['gte'] == '2013-6')
+        assert(filter_dsl['and'][0]['range']['date']['lte'] == '2014-6')
