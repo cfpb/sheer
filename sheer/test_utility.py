2774ef0d02bb2aa859a5deb9aaef633c10e470f0
Ross Karchner <ross.karchner@cfpb.gov>
2014-07-24 18:19:56 -0400

    removed a PDB

--- sheer/test_utility.py
+++ sheer/test_utility.py
@@ -13,7 +13,6 @@ class testSearchpaths:
 
     def test_searchpath_with_append(self):
         paths = build_search_path('/var/sheer', '/my/site/is/cool/foo.html', append='_layouts', include_start_directory=False)
-        import pdb;pdb.set_trace()
         assert('/var/sheer/my/site/_layouts' in paths)
         assert('/var/sheer/my/_layouts' in paths)
         assert('/var/sheer/_layouts' in paths)
