134f09b33cf139d07f4d2b70cdf6b2c5cb26f95f
Ross Karchner <ross.karchner@cfpb.gov>
2014-11-13 21:22:22 -0500

    Deleting old view code

--- sheer/views.py
+++ sheer/views.py
@@ -33,7 +33,12 @@ def handle_request(lookup_name=None, lookup_config=None, **kwargs):
     if request_path.endswith('/'):
             request_path += ('index.html')
 
+    
     translated_path = os.path.join(root_dir, request_path[1:])
+
+    if os.path.isdir(translated_path) and not request_path.endswith('/'):
+            return flask.redirect(request_path+'/')
+
 
     if not request_path.endswith('.html') and os.path.exists(translated_path):
         flask.send_file(translated_path)
@@ -79,40 +84,3 @@ def serve_error_page(error_code):
             return flask.render_template_string(template_source.read()), error_code
     else:
         return "Please provide a %s.html!" % error_code
-
-# legacy view code, hopefully to be deleted
-def serve_requested_file(directory, requested_file, lookup_results=None):
-    complete_path = os.path.join(directory, requested_file)
-    if os.path.exists(complete_path):
-        if requested_file.lower().endswith('.html'):
-            pass
-        else:
-            return flask.send_from_directory(directory, requested_file)
-    elif lookup_results:
-        lookup_name, url_args = lookup_results
-        url_args['name'] = lookup_name
-        optional_custom_template = url_args['id'] + ".html"
-        if os.path.exists(os.path.join(directory, optional_custom_template)):
-            template_name = optional_custom_template
-        else: 
-            template_name = '_single.html'
-        template_path = os.path.join(directory, template_name)
-        # do we need to initialize extra_context?
-        extra_context = {}
-        extra_context = flask.current_app.url_lookups_by_name[lookup_name](url_args)
-        if os.path.exists(template_path):
-            with codecs.open(template_path, encoding="utf-8") as template_source:
-                return flask.render_template_string(template_source.read(),**extra_context)
-    flask.abort(404)
-
-            
-
-def old_handle_request(diskpath, remainder):
-    requested_file = remainder or 'index.html'
-    lookup_map = flask.current_app.lookup_map.bind_to_environ(flask.request.environ)
-    try:
-        lookup_result = lookup_map.match()
-    except NotFound:
-        lookup_result = None
-        
-    return serve_requested_file(diskpath, requested_file, lookup_results=lookup_result)
