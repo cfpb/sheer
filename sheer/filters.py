b03a3d8c7fede38111f16ab8e03f098010fa2d4b
Dan Ford <dpford@gmail.com>
2015-02-19 16:58:54 -0500

    reworked bool filters to make more sense for real world usage

--- sheer/filters.py
+++ sheer/filters.py
@@ -4,65 +4,75 @@ import calendar
 import datetime
 from dateutil.parser import parse
 
+def generate_term_filters(multidict, filter_keys):
+    term_main = {"and": []}
+    for key in filter_keys:
+        field = key.replace('filter_', '')
+        filter_type_main = {"or": []}
+        values = multidict.getlist(key)
+        for val in values:
+            term_single = {"term": {}}
+            term_single["term"][field] = val
+            filter_type_main["or"].append(term_single)
+        term_main["and"].append(filter_type_main)
+    return term_main
+
+def generate_range_filters(multidict, filter_keys):
+    range_clause = {"range": {}}
+    for key in filter_keys:
+        full_field = key.replace('filter_range_', '')
+        # We account for potential underscores in the field name itself
+        # e.g. comment_count
+        operator = full_field[full_field.rfind('_') + 1:]
+        field = full_field[:full_field.rfind('_')]
+        if field not in range_clause["range"]:
+            range_clause["range"][field] = {}
+        # If there are multiples of the same date filter, this will take
+        # the first
+        value = multidict.getlist(key)[0]
+        range_clause["range"][field][operator] = value
+
+    # Validate date range input
+
+    # First check if both date_lte and date_gte are present
+    # If the 'start' date is after the 'end' date, swap them
+    if 'date' in range_clause['range']:
+        if all(x in range_clause['range']['date'] for x in ('lte', 'gte')) and \
+            parse(range_clause['range']['date']['gte'], default=datetime.date.today().replace(day=1)) > \
+                parse(range_clause['range']['date']['lte'], default=datetime.date.today().replace(day=1)):
+            range_clause['range']['date']['gte'], range_clause['range']['date']['lte'] = \
+                range_clause['range']['date'][
+                    'lte'], range_clause['range']['date']['gte']
+        # If either date matches the YYYY-M[M] format, append the
+        # appropriate day
+        if 'lte' in range_clause['range']['date'] and \
+                re.compile("^[0-9]{4}-[0-9]{1,2}$").match(range_clause['range']['date']['lte']):
+            year, month = range_clause['range']['date']['lte'].split('-')
+            last_day_of_month = calendar.monthrange(
+                int(year), int(month))[1]
+            range_clause['range']['date'][
+                'lte'] += "-{0}".format(last_day_of_month)
+        if 'gte' in range_clause['range']['date'] and \
+                re.compile("^[0-9]{4}-[0-9]{1,2}$").match(range_clause['range']['date']['gte']):
+            range_clause['range']['date']['gte'] += "-1"
+    return range_clause
+
+
 
 def filter_dsl_from_multidict(multidict):
-    # Split the filters between 'range' and 'bool', making sure the query
+    # Split the filters between 'range' and 'term', making sure the query
     # value isn't blank
-    bool_filter_keys = [r for r in [k for k in multidict.keys() if re.compile("^filter_(?!range_)").match(k)]
+    term_filter_keys = [r for r in [k for k in multidict.keys() if re.compile("^filter_(?!range_)").match(k)]
                         if multidict[r]]
     range_filter_keys = [r for r in [k for k in multidict.keys() if re.compile("^filter_range_").match(k)]
                          if multidict[r]]
-
     final_filters = []
-    if bool_filter_keys:
-        bool_clause = {"bool": {"must": [], "should": [], "must_not": []}}
-        for key in bool_filter_keys:
-            field = key.replace('filter_', '')
-            values = multidict.getlist(key)
-            for val in values:
-                term_clause = {"term": {}}
-                term_clause["term"][field] = val
-                bool_clause["bool"]["should"].append(term_clause)
-        final_filters.append(bool_clause)
-
+    if term_filter_keys:
+        term_clause = generate_term_filters(multidict, term_filter_keys)
+        final_filters.append(term_clause)
+        
     if range_filter_keys:
-        range_clause = {"range": {}}
-        for key in range_filter_keys:
-            full_field = key.replace('filter_range_', '')
-            # We account for potential underscores in the field name itself
-            # e.g. comment_count
-            operator = full_field[full_field.rfind('_') + 1:]
-            field = full_field[:full_field.rfind('_')]
-            if field not in range_clause["range"]:
-                range_clause["range"][field] = {}
-            # If there are multiples of the same date filter, this will take
-            # the first
-            value = multidict.getlist(key)[0]
-            range_clause["range"][field][operator] = value
-
-        # Validate date range input
-
-        # First check if both date_lte and date_gte are present
-        # If the 'start' date is after the 'end' date, swap them
-        if 'date' in range_clause['range']:
-            if all(x in range_clause['range']['date'] for x in ('lte', 'gte')) and \
-                parse(range_clause['range']['date']['gte'], default=datetime.date.today().replace(day=1)) > \
-                    parse(range_clause['range']['date']['lte'], default=datetime.date.today().replace(day=1)):
-                range_clause['range']['date']['gte'], range_clause['range']['date']['lte'] = \
-                    range_clause['range']['date'][
-                        'lte'], range_clause['range']['date']['gte']
-            # If either date matches the YYYY-M[M] format, append the
-            # appropriate day
-            if 'lte' in range_clause['range']['date'] and \
-                    re.compile("^[0-9]{4}-[0-9]{1,2}$").match(range_clause['range']['date']['lte']):
-                year, month = range_clause['range']['date']['lte'].split('-')
-                last_day_of_month = calendar.monthrange(
-                    int(year), int(month))[1]
-                range_clause['range']['date'][
-                    'lte'] += "-{0}".format(last_day_of_month)
-            if 'gte' in range_clause['range']['date'] and \
-                    re.compile("^[0-9]{4}-[0-9]{1,2}$").match(range_clause['range']['date']['gte']):
-                range_clause['range']['date']['gte'] += "-1"
+        range_clause = generate_range_filters(multidict, range_filter_keys)
         final_filters.append(range_clause)
     return final_filters
 
